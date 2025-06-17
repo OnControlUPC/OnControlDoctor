2853b421c4891687905bcd415ccd4a8ce7429850
JJPPRRMM <jjpprrmm@gmail.com>
2025-06-16 22:07:49 -0500

    fix: Base_url fixed

--- app/src/main/java/oncontroldoctor/upc/edu/pe/shared/data/remote/ApiConstants.kt
+++ app/src/main/java/oncontroldoctor/upc/edu/pe/shared/data/remote/ApiConstants.kt
@@ -1,5 +1,5 @@
 package oncontroldoctor.upc.edu.pe.shared.data.remote
 
 object ApiConstants {
-    const val BASE_URL = " https://44f2-2800-4b0-4501-b819-a09b-613c-6384-51e0.ngrok-free.app/api/v1/"
+    const val BASE_URL = "https://7128-38-187-27-247.ngrok-free.app/api/v1/"
 }
